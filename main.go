68ca26369e79a9a9d1b7670c1d7996814dec0c27
sagaraEito <gjmptw.aab@gmail.com>
2022-07-18 14:32:37 +0900

    First commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"ToDoList-Server/model"
+	"ToDoList-Server/router"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	sqlDB := model.DBConnection()
+	defer sqlDB.Close()
+	e := echo.New()
+	router.SetRouter(e)
+}
---
+++ router/router.go
@@ -0,0 +1,40 @@
+package router
+
+import (
+	"os"
+
+	"github.com/labstack/echo/v4/middleware"
+
+	_ "net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+//Routingを設定する関数　引数はecho.echo型であり、戻り値はerror型
+func SetRouter(e *echo.Echo) error {
+
+	// 諸々の設定(*1)
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "${time_rfc3339_nano} ${host} ${method} ${uri} ${status} ${header}\n",
+		Output: os.Stdout,
+	}))
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORS())
+	e.PUT("/api/tasks/:taskID", ChangeFinishedTaskHandler)
+
+	// APIを書く場所
+	api := e.Group("/api")
+	{
+		apiTasks := api.Group("/tasks")
+		{
+			apiTasks.GET("", GetTasksHandler)
+			apiTasks.POST("", AddTaskHandler)
+			apiTasks.PUT("/:taskID", ChangeFinishedTaskHandler)
+			apiTasks.DELETE("/:taskID", DeleteTaskHandler)
+		}
+	}
+	
+	// 8000番のポートを開く(*2)
+	err := e.Start(":8000")
+	return err
+}
